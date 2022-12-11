b7a67df0405d37d47fa8ceec11cb28822a5243c5
Jason Chung <jach7279@colorado.edu>
2022-12-11 11:02:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# porfolio-website
+ Personal/Portfolio website build using React
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    title: "RecipEat",
+    subtitle: "HTML, CSS, Javascript, EJS, PostreSQL, Node, Docker",
+    description:
+      "Group project for software development class that was designed to help users search for recipes based on what's in their pantry and dietary preferences.",
+    image: "./project-1.PNG",
+    link: "https://drive.google.com/file/d/1_p3i7UntBl5MugVGJrMbilF0QAqj9Gyp/view",
+  },
+  {
+    title: "Virus Spread across Different Networks",
+    subtitle: "MATLAB and LaTeX",
+    description:
+      "This project is focused on analysing how a SARS virus spread over different types of networks. Simulations were run in MATLAB and the writeup was done in LaTeX.",
+    image: "./project-2.PNG",
+    link: "https://github.com/Jaesunee/Discrete-Math-Project",
+  },
+  {
+    title: "Volleyball Store Page",
+    subtitle: "HTML and CSS",
+    description:
+      "A webpage meant for a Volleyball brand built from scratch using HTML and CSS",
+    image: "./project-3.PNG",
+    link: "https://Jaesunee.github.io",
+  },
+  {
+    title: "Personal Website",
+    subtitle: "React JS App",
+    description:
+      "This website is a React JS application deployed to Github. Click to navigate to a tutorial where you can learn to build your own.",
+    image: "./react.png",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "C/C++",
+  "Python",
+  "Java",
+  "Javascript",
+  "HTML/CSS",
+  "React",
+  "Node",
+  "SQL",
+  "Docker",
+  "bash",
+  "Git"
+];
