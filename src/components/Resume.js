551ad44f3af73d6c04b3410668ff82ba2e4b0cb0
Jason Chung <jach7279@colorado.edu>
2023-08-07 04:19:39 -0600

    update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,20 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function Resume() {
     return (
         <section id="resume">
-          <div className="container px-5 py-10 mx-auto text-center items-center">
-            <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
-              Resume
-            </h1>
-            <br></br>
-            <div className="flex flex-wrap lg:w-5/5 sm:mx-auto sm:mb-2 -mx-2 items-center justify-center">
-                <img
-                    alt="CV"
-                    src="./resume.jpg"
-                />
+            <div className="container px-5 py-10 mx-auto text-center items-center">
+                <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+                    Resume
+                </h1>
+                <br></br>
+                <div className="flex flex-wrap lg:w-5/5 sm:mx-auto sm:mb-2 -mx-2 items-center justify-center">
+                    <img alt="CV" src="/resume.jpg" />
+                </div>
             </div>
-          </div>
         </section>
-      );
+    );
 }
