426e6f240fffcfbbbe6c5721431d036ccf017819
Jason Chung <jasonchung3003@gmail.com>
2024-10-30 20:20:09 -0400

    update
    prevent shift of content with type animation

--- src/components/About.js
+++ src/components/About.js
@@ -29,48 +29,43 @@ export default function About() {
         };
     }, []);
     return (
-        <section className="wrapper"id="about" data-aos-once="true">
+        <section className="wrapper" id="about" data-aos-once="true">
             <div className=" container mx-auto transform scale-90 flex px-20 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white text-title" data-aos="zoom-out">
-                        Hi, my name is <span className='text-green-500'>Jason Chung</span>.
-                        <br className="sm:block"></br>
-                        <div data-aos="fade leading-relaxed" data-aos-delay="1000">
-                            
-                            <div>
-                                <TypeAnimation
-                                    preRenderFirstString={true}
-                                    sequence={[
-                                        1500,
-                                        'I am a Software Development Engineer.',
-                                        1000,
-                                        'I am a Student in Software Engineering.',
-                                        1000,
-                                        'I am an ML/AI Engineer.',
-                                        1000,
-                                        'I am a Videogame Enthusiast.',
-                                        1000,
-                                        'I am a Proud Korean-American.',
-                                        1000,
-                                        'I am a Volleyball Player.',
-                                        500
-                                      ]}
-                                    speed={50}
-                                    style={{ fontSize: '1em' }}
-                                    repeat={Infinity}
-                                />
-                            </div>
+                        Hi, my name is <span className="text-green-500">Jason Chung</span>.<br className="sm:block"></br>
+                        <div data-aos="fade leading-relaxed" data-aos-delay="1000" style={{ minWidth: '680px' }}>
+                            <TypeAnimation
+                                preRenderFirstString={true}
+                                sequence={[
+                                    1500,
+                                    'I am a Software Developer.',
+                                    1000,
+                                    'I am a Software Engineering Student.',
+                                    1000,
+                                    'I am an ML/AI Engineer.',
+                                    1000,
+                                    'I am a Videogame Enthusiast.',
+                                    1000,
+                                    'I am a Proud Korean-American.',
+                                    1000,
+                                    'I am a Volleyball Player.',
+                                    500,
+                                ]}
+                                speed={50}
+                                style={{ fontSize: '1em' }}
+                                repeat={Infinity}
+                            />
                         </div>
                     </h1>
                     <p className="mb-8 leading-relaxed text-base" data-aos="fade" data-aos-delay="2000">
-                        <br></br>I'm pursuing a Master's in Software Engineering at Carnegie Mellon University,
-                         having earned my BS in Computer Science from the University of Colorado Boulder. 
-                         I enjoy building applications at the intersection of computer science, ML/AI, and Big Data. 
-                         This passion has led me to several internship opportunities with various organizations. 
-                         Wherever I go, I strive to create a diverse, equitable, and inclusive team environment.
-                        <br></br><br></br>
-                        I am currently seeking an internship role for next summer (2025) and full-time opportunities for the start of
-                        2026 related to software engineering.
+                        <br></br>I'm pursuing a Master's in Software Engineering at Carnegie Mellon University, having earned my BS in Computer Science from the
+                        University of Colorado Boulder. I enjoy building applications at the intersection of computer science, ML/AI, and Big Data. This passion
+                        has led me to several internship opportunities with various organizations. Wherever I go, I strive to create a diverse, equitable, and
+                        inclusive team environment.
+                        <br></br>
+                        <br></br>I am currently seeking an internship role for next summer (2025) and full-time opportunities for the start of 2026 related to
+                        software engineering.
                         <br></br>
                     </p>
                     <div className="flex justify-center" data-aos="fade" data-aos-delay="3000">
